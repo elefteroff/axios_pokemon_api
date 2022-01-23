a6ccd994b417f12700995328daff5ed214e5f8ca
elefteroff <elefteroff@mac.com>
2022-01-23 13:00:50 -0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [pokemon, setPokemon] = useState([]);
+
+  const getAxiosPokemon = () => {
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=807")
+      .then(response => {
+        console.log(response.data)
+        setPokemon([response.data])
+      })
+      .catch(error => console.log(error))
+  }
+
+  // const pokemonIndex = pokemon.map(i => i.idx)
+  // const pokemonResults = pokemonIndex.map(r => r.results)
+  // const pokemonNames = pokemonResults.map(n => n.name)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>List of Pokemon</h2>
+      <button onClick={getAxiosPokemon}>Fetch Pokemon</button>
+      <hr />
+      <table>
+        <thead>
+          <tr>
+            <th>Pokemon Names</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            pokemon.map( (pname) => {
+              return (
+                <tr key={pname}>
+                  <td>{pname.name}</td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      </table>
     </div>
   );
 }
